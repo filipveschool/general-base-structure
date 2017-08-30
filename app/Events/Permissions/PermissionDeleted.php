2ac822daeebeddf2522df5b71aefd1cb594d2d40
deb97908 <filipve@outlook.com>
2017-08-30 23:31:39 +0200

    add events

---
+++ app/Events/Permissions/PermissionDeleted.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Events\Permissions;
+
+use Illuminate\Broadcasting\Channel;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Broadcasting\PrivateChannel;
+use Illuminate\Broadcasting\PresenceChannel;
+use Illuminate\Foundation\Events\Dispatchable;
+use Illuminate\Broadcasting\InteractsWithSockets;
+use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
+
+class PermissionDeleted
+{
+    use Dispatchable, InteractsWithSockets, SerializesModels;
+
+    public $permission;
+
+    /**
+     * Create a new event instance.
+     *
+     * @param $permission
+     */
+    public function __construct($permission)
+    {
+        $this->permission = $permission;
+    }
+
+    /**
+     * Get the channels the event should broadcast on.
+     *
+     * @return \Illuminate\Broadcasting\Channel|array
+     */
+    public function broadcastOn()
+    {
+        return new PrivateChannel('channel-name');
+    }
+}
---
+++ app/Events/Roles/RoleCreated.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Events\Roles;
+
+use Illuminate\Broadcasting\Channel;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Broadcasting\PrivateChannel;
+use Illuminate\Broadcasting\PresenceChannel;
+use Illuminate\Foundation\Events\Dispatchable;
+use Illuminate\Broadcasting\InteractsWithSockets;
+use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
+
+class RoleCreated
+{
+    use Dispatchable, InteractsWithSockets, SerializesModels;
+
+    public $role;
+
+    /**
+     * Create a new event instance.
+     *
+     * @param $role
+     */
+    public function __construct($role)
+    {
+        $this->role = $role;
+    }
+
+    /**
+     * Get the channels the event should broadcast on.
+     *
+     * @return \Illuminate\Broadcasting\Channel|array
+     */
+    public function broadcastOn()
+    {
+        return new PrivateChannel('channel-name');
+    }
+}
